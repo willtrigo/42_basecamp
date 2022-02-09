a16d73adca3b58b28177b755ebf89c16dd4edf16
dade-jes <willtrigo@gmail.com>
2022-02-09 23:05:51 +0100

    Created file ft_print_reverse_alphabet.c

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/09 22:53:03 by dade-jes          #+#    #+#             */
+/*   Updated: 2022/02/09 23:04:24 by dade-jes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		write(1, &alphabet, 1);
+		alphabet--;
+	}
+}
