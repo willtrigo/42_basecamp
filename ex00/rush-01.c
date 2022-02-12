a5c7803cfb770c16d8f8bf0bd8e9cc230ec47dbe
dade-jes <willtrigo@gmail.com>
2022-02-12 22:52:49 +0100

    Fixed, remove breaklines and define ERR.

--- ex00/rush-01.c
+++ ex00/rush-01.c
@@ -6,22 +6,18 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 00:50:10 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/12 16:30:44 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/12 22:47:15 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_check_map(char *str);
 void	ft_putchar(int i, int breakline, char *c);
 
-#define ERR "Error"
-
 int	main(int argc, char *argv[])
 {
 	if (argc == 2)
-	{
 		ft_check_map(argv[1]);
-	}
 	else
-		ft_putchar(5, 1, ERR);
+		ft_putchar(5, 1, "Error");
 	return (0);
 }
