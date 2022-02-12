9276f5e10c665712cb6aac75d7c55612f160afbc
dade-jes <willtrigo@gmail.com>
2022-02-12 18:15:09 +0100

    Created logic of the get args and check if the arg was valid or not.

--- ex00/ft_check_map.c
+++ ex00/ft_check_map.c
@@ -6,7 +6,7 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 05:29:07 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/12 06:50:39 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/12 18:13:07 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int		ft_strlen(char *str);
 void	ft_putchar(int i, int breakline, char *c);
+void	ft_create_game(char *resolution);
 
 #define MAP_SIZE_3X3 23
 #define MAP_SIZE_4X4 31
@@ -27,18 +28,15 @@ void	ft_check_map(char *str)
 	len_str = ft_strlen(str);
 	if (len_str == MAP_SIZE_3X3)
 	{
-		printf("args - %s\n", str);
-		ft_putchar(1, 1, "");
+		ft_create_game("1 2 3 4 \n 2 3 4 1 \n  3 4 1 2 \n 4 1 2 3 \n");
 	}
 	else if (len_str == MAP_SIZE_4X4)
 	{
-		printf("args - %s\n", str);
-		ft_putchar(1, 1, "");
+		ft_create_game("1 2 3 4 \n 2 3 4 1 \n  3 4 1 2 \n 4 1 2 3 \n");
 	}
 	else if (len_str == MAP_SIZE_5X5)
 	{
-		printf("args - %s	\n", str);
-		ft_putchar(1, 1, "");
+		ft_create_game("1 2 3 4 \n 2 3 4 1 \n  3 4 1 2 \n 4 1 2 3 \n");
 	}
 	else
 		ft_putchar(5, 1, ERR);
--- ex00/ft_create_game.c
+++ ex00/ft_create_game.c
@@ -6,7 +6,21 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 05:42:06 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/12 05:42:08 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/12 18:11:33 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_putchar(int i, int breakline, char *c);
+
+void	ft_create_game(char	*resolution)
+{
+	int		i;
+
+	i = 0;
+	while (resolution[i] != '\0')
+	{
+		if (resolution[i] != ' ')
+			ft_putchar(1, 0, &resolution[i]);
+		i++;
+	}
+}
