0d4bffd092a15cff835ae36f4d58341cd87837e9
hbernard <hbernard@student.42sp.org.br>
2022-02-13 22:16:18 +0100

    WIP , o nome da variavel MAP_SIZE para BEHOLDER para melhor legibilidade e adicionamos mais um argumento para passar para o ft_create_game.

--- ex00/ft_check_map.c
+++ ex00/ft_check_map.c
@@ -3,38 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_check_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
+/*   By: hbernard <hbernard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 05:29:07 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/13 06:58:56 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/13 22:07:33 by hbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_check_numbers(int len, int max, char *str);
 int		ft_strlen(char *str);
 void	ft_putchar(int i, int breakline, char *c);
-void	ft_create_game(int type_size, char *str);
+void	ft_create_game(int type_size, char *str, int len_str);
 
-#define MAP_SIZE_3X3 17
-#define MAP_SIZE_4X4 31
-#define MAP_SIZE_5X5 49
+#define BEHOLDER_3X3 23//4 * (3 * 2 ) - 1
+#define BEHOLDER_4X4 31//4 * (4 * 2 ) - 1
+#define BEHOLDER_5X5 39//4 * (5 * 2 ) - 1
 
 void	ft_check_map(char *str)
 {
 	int	len_str;
 
 	len_str = ft_strlen(str);
-	if (len_str == MAP_SIZE_3X3 && ft_check_numbers(len_str, 3, str))
+	if (len_str == BEHOLDER_3X3 && ft_check_numbers(len_str, 3, str))
 	{
-		ft_create_game(3, str);
+		ft_create_game(3, str, len_str);
 	}
-	else if (len_str == MAP_SIZE_4X4 && ft_check_numbers(len_str, 4, str))
+	else if (len_str == BEHOLDER_4X4 && ft_check_numbers(len_str, 4, str))
 	{
-		ft_create_game(4, str);
+		ft_create_game(4, str, len_str);
 	}
-	else if (len_str == MAP_SIZE_5X5 && ft_check_numbers(len_str, 5, str))
+	else if (len_str == BEHOLDER_5X5 && ft_check_numbers(len_str, 5, str))
 	{
-		ft_create_game(5, str);
+		ft_create_game(5, str, len_str);
 	}
 	else
 		ft_putchar(5, 1, "Error");
