f74a885f17afcf87fb1e7709f15224809170ca8b
dade-jes <willtrigo@gmail.com>
2022-02-18 20:55:01 +0100

    Fixed the norminette in ex02 and ex03.

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 15:03:03 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 18:00:44 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 20:54:12 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int	ft_str_is_alpha(char *str)
 	i = -1;
 	while (str[++i])
 		if ((str[i] > 'Z' || str[i] < 'A') && (str[i] > 'z' || str[i] < 'a'))
-			return 0;
-	return 1;
+			return (0);
+	return (1);
 }
--- ex03/ft_str_is_numeric.c
+++ ex03/ft_str_is_numeric.c
@@ -6,19 +6,19 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 18:32:52 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 20:49:37 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 20:53:53 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int ft_str_is_numeric(char *str)
+int	ft_str_is_numeric(char *str)
 {
 	int	i;
 
 	i = -1;
 	while (str[++i])
 		if (str[i] < 0 || str[i] > 9)
-			return 0;
-	return 1;
+			return (0);
+	return (1);
 }
