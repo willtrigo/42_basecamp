793d7d522cc391f7d44eccecfb5d52ebf8d97bc6
dade-jes <willtrigo@gmail.com>
2022-02-18 05:56:06 +0100

    Created function that reverse the position of the ints.

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,7 +6,30 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 03:54:41 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 03:54:42 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 05:48:13 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	swap_tab(int *a, int *b);
+
+void 	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		swap_tab(&tab[i], &tab[size -1]);
+		i++;
+		size--;
+	}
+}
+
+void	swap_tab(int *a, int *b)
+{
+	int	swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
