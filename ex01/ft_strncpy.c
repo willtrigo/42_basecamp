0870003195d2488f84e055b3acd1e1a210f4f3d5
dade-jes <willtrigo@gmail.com>
2022-02-18 14:49:36 +0100

    Created the function that changes the char with a delimiter.

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,22 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 13:40:35 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 13:40:36 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 14:29:54 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[n] = '\0';
+	return (dest);
+}
