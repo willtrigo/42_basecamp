bf69b17b41717659b0a3c7fe0101c5870c40d838
dade-jes <willtrigo@gmail.com>
2022-02-19 00:20:00 +0100

    Fixed logic of the strncopy, missing blank spaces in the end of the word, now is completed with the correct chars.

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 13:40:35 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 15:04:14 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 23:59:15 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	dest[n] = '\0';
