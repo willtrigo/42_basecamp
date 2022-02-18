bee0405f456582c387406b62ca6e73a846c3782f
dade-jes <willtrigo@gmail.com>
2022-02-18 08:39:49 +0100

    Fixed white space with the norminette in the ft_rev_int_tab.c

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,13 +6,13 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 03:54:41 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 05:48:13 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 08:38:36 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	swap_tab(int *a, int *b);
 
-void 	ft_rev_int_tab(int *tab, int size)
+void	ft_rev_int_tab(int *tab, int size)
 {
 	int	i;
 
