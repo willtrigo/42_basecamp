014a5e22d2463fd88a7d83127c0c0bccf9d8e976
dade-jes <willtrigo@gmail.com>
2022-02-18 18:31:30 +0100

    Created the function that check if the str has only alphabetical chars.

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,7 +6,17 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 15:03:03 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 15:03:04 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 18:00:44 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		if ((str[i] > 'Z' || str[i] < 'A') && (str[i] > 'z' || str[i] < 'a'))
+			return 0;
+	return 1;
+}
