c76d4d2460696f0a50e285ada6a9e39123b1f0e2
dade-jes <willtrigo@gmail.com>
2022-02-18 13:43:47 +0100

    Created the function that copy the value of a string to the other.

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,7 +6,20 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 09:35:32 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 09:35:34 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 12:36:53 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
