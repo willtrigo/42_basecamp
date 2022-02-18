2a7cdb6166a515b82f5dd2f0980c236dbe0d86d2
dade-jes <willtrigo@gmail.com>
2022-02-18 03:08:22 +0100

    Created function that receive a char and print them.

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -6,7 +6,27 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 02:12:30 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/18 02:12:31 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/18 03:02:29 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
