b71be038fd1dbe5834e7caef0a8879601f5da2e1
dade-jes <willtrigo@gmail.com>
2022-02-19 02:35:58 +0100

    Created the function str upper.

--- ex07/ft_strupcase.c
+++ ex07/ft_strupcase.c
@@ -6,7 +6,20 @@
 /*   By: dade-jes <willtrigo@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 00:52:37 by dade-jes          #+#    #+#             */
-/*   Updated: 2022/02/19 00:52:39 by dade-jes         ###   ########.fr       */
+/*   Updated: 2022/02/19 02:21:41 by dade-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <stdio.h>
+
+char *ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[++i])
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		i++;
+	return (str);
+}
